e439198b3939fc231a65a6bff1af92546ae72c7b
erikacunanan29 <erikacunanan29@gmail.com>
2017-11-09 09:43:57 +0800

    Add files via upload
    Multiple Client Chat Module

---
+++ ChatClient.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+import java.net.*;
+import java.io.*;
+
+public class ChatClient implements Runnable {
+	static Socket clientSocket = null;
+	static DataOutputStream outStream = null;
+	static DataInputStream inStream = null;
+	static Scanner reader = new Scanner(System.in);
+	static boolean offline = false;
+
+	public static void main(String args[]) {
+		int portNumber = 2222;
+		String host = "localhost";
+
+		//gets host (ip address) and port number
+		try {
+			System.out.print("Enter host: "); 
+			host = reader.nextLine();
+
+			System.out.print("Enter port number: ");
+			portNumber = reader.nextInt();
+
+			//creates a socket based on the given host and port number
+			clientSocket = new Socket(host, portNumber);
+			System.out.println("\nConnected to " + host + " on port " + portNumber + ".");
+
+			//initiliazes input and output stream
+			reader = new Scanner(new InputStreamReader(System.in));
+			outStream = new DataOutputStream(clientSocket.getOutputStream()); //for sending data to server
+			inStream = new DataInputStream(clientSocket.getInputStream()); //for receiving data from server
+		} catch (UnknownHostException uhe) {
+			System.out.println("Unknown host " + host);
+		} catch (IOException ie) {
+			System.out.println("I/O not found.");
+		}
+
+		if (clientSocket != null && outStream != null && inStream != null) {
+			try {
+				//creates a thread to read from server
+				new Thread(new ChatClient()).start();
+
+				//sends data read to server
+				while (offline != true) {
+					outStream.writeUTF(reader.nextLine().trim());
+				} 
+
+				//close socket, dataoutputstream, datainputstream
+				outStream.close();
+				inStream.close();
+				clientSocket.close();
+			} catch (IOException ie) {
+				System.out.println("IOException + " + ie);
+			}
+		}
+	}
+
+	public void run() {
+		String inputText;
+
+		//keep on reading from socket and printing it til "\quit" is received from server
+		try {
+			while((inputText = inStream.readUTF()) != null) {
+				System.out.println(inputText);
+				if (inputText == "Goodbye.") {
+					break;
+				}
+			}
+			offline = true;
+		} catch (IOException ie) {
+			System.out.println(ie);
+		}
+	}
+}
+
---
+++ ClientThread.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+import java.net.*;
+import java.io.*;
+
+class ClientThread extends Thread {
+	DataInputStream inStream = null;
+	DataOutputStream outStream = null;
+	Socket clientSocket = null;
+	ClientThread[] threads;
+	int maxClients;
+
+	public ClientThread(Socket clientSocket, ClientThread[] threads) {
+		this.clientSocket = clientSocket;
+		this.threads = threads;
+		maxClients = threads.length;
+	}
+
+	public void run() {
+		int maxClients = this.maxClients;
+		ClientThread[] threads = this.threads;
+
+		try {
+
+			//create datainputstream and dataoutputstream
+			inStream = new DataInputStream(clientSocket.getInputStream());
+			outStream = new DataOutputStream(clientSocket.getOutputStream());
+
+			//gets the name of the client and welcomes him/her
+			outStream.writeUTF("Enter your name: ");
+			String name = inStream.readUTF().trim();
+
+			outStream.writeUTF("\n\tWelcome " + name + " to the chatroom.\n\tEnter \\quit in order to leave.");
+
+			//broadcasts to all clients (except the newly created one) that a new client has been added
+			synchronized (this) {
+				for(int i=0; i < maxClients; i++) {
+					if (threads[i] != null && threads[i] != this) {
+						threads[i].outStream.writeUTF("\n\t>> New user " + name + " has entered the chatroom.\n");
+					} 
+				}
+			}
+
+			//gets message from clients, breaks when the client enters the keyword 'quit'
+			while(true) {
+				String input = inStream.readUTF();
+				if (input.startsWith("\\quit")) {
+					break;
+				} 
+
+				//displays message from a client to all clients
+				synchronized (this) {
+					for(int i=0; i < maxClients; i++) {
+						if (threads[i] != null) {
+							threads[i].outStream.writeUTF("> " + name + ": " + input);
+						}
+					}
+				}
+			}
+
+			//if a client types the keyword, tell all other clients that the client has left
+			synchronized (this) {
+				for(int i=0; i < maxClients; i++) {
+					if (threads[i] != null && threads[i] != this) {
+						threads[i].outStream.writeUTF("User " + name + " left the chatroom.");
+					} 
+				}
+			}
+			outStream.writeUTF("Goodbye.");
+
+			//clear the threads index array for the client that left so that others can use it
+			synchronized (this) {
+				for(int i=0; i < maxClients; i++) {
+					if (threads[i] == this) {
+						threads[i] = null;
+					}
+				}
+			}
+
+			inStream.close();
+			outStream.close();
+			clientSocket.close();
+		} catch (IOException io) {
+			System.out.println(io);
+		}
+	}
+}
